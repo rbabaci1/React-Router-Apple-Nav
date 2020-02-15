44d219064c58475c20f0bd0c63510c6e74c7eea2
rabah babaci <4rabah@gmail.com>
2020-02-14 17:59:21 -0800

    set the state for the data

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -5,6 +5,8 @@ import "./App.css";
 import appleData from "./data/appleData";
 
 function App() {
+  const [products] = useState(appleData);
+
   return (
     <div className="App">
       <h2>test</h2>
