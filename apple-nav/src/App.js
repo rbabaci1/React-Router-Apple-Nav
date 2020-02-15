4f80bbab69cc0e2ee5b2c695b4f01b767a833fd6
rabah babaci <4rabah@gmail.com>
2020-02-14 17:53:36 -0800

    create the product data

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h2>Test</h2>
+      <h2>test</h2>
     </div>
   );
 }
---
+++ apple-nav/src/data/appleData.js
@@ -0,0 +1,93 @@
+const productData = [
+  {
+    id: 1,
+    name: "Mac",
+    repository: [
+      { id: 1, name: "MacBook Air", new: false },
+      { id: 2, name: 'MacBook Pro 13"', new: false },
+      { id: 3, name: 'MacBook Pro 16"', new: true },
+      { id: 4, name: "iMac", new: false },
+      { id: 5, name: "iMac Pro", new: false },
+      { id: 6, name: "Mac Pro", new: true },
+      { id: 7, name: "Mac mini", new: false },
+      { id: 8, name: "Compare", new: false },
+      { id: 9, name: "Pro Display XDR", new: true },
+      { id: 10, name: "Accessories", new: false },
+      { id: 11, name: "Catalina", new: false }
+    ]
+  },
+  {
+    id: 2,
+    name: "iPad",
+    repository: [
+      { id: 1, name: "iPad Pro", new: false },
+      { id: 2, name: "iPad Air", new: false },
+      { id: 3, name: "iPad", new: true },
+      { id: 4, name: "iPad mini", new: false },
+      { id: 5, name: "Compare", new: false },
+      { id: 6, name: "Apple Pencil", new: false },
+      { id: 7, name: "Smart Keyboard", new: false },
+      { id: 8, name: "AirPods", new: true },
+      { id: 9, name: "Accessories", new: false },
+      { id: 10, name: "iPadOS", new: false }
+    ]
+  },
+  {
+    id: 3,
+    name: "iPhone",
+    repository: [
+      { id: 1, name: "iPhone 11 Pro", new: true },
+      { id: 2, name: "iPhone11", new: true },
+      { id: 3, name: "iPhone Xr", new: false },
+      { id: 4, name: "iPhone 8", new: false },
+      { id: 5, name: "Compare", new: false },
+      { id: 6, name: "Apple Card", new: false },
+      { id: 8, name: "AirPods", new: true },
+      { id: 9, name: "Accessories", new: false },
+      { id: 10, name: "iOS 13", new: false }
+    ]
+  },
+  {
+    id: 4,
+    name: "Watch",
+    repository: [
+      { id: 1, name: "Apple Watch Series 5", new: true },
+      { id: 2, name: "Apple Watch Nike", new: true },
+      { id: 3, name: "Apple Watch Hermès", new: true },
+      { id: 4, name: "Apple Watch Edition", new: true },
+      { id: 5, name: "Apple Watch Series 3", new: false },
+      { id: 6, name: "Compare", new: false },
+      { id: 8, name: "Bands", new: false },
+      { id: 9, name: "AirPods", new: true },
+      { id: 10, name: "Accessories", new: false },
+      { id: 10, name: "WatchOS", new: false }
+    ]
+  },
+  {
+    id: 5,
+    name: "TV",
+    repository: [
+      { id: 1, name: "Apple TV+", new: true },
+      { id: 2, name: "Apple TV app", new: true },
+      { id: 3, name: "Apple TV 4K", new: false },
+      { id: 4, name: "Apple TV HD", new: false },
+      { id: 5, name: "AirPlay", new: false },
+      { id: 10, name: "Accessories", new: false }
+    ]
+  },
+  {
+    id: 5,
+    name: "Music",
+    repository: [
+      { id: 1, name: "Apple Music", new: false },
+      { id: 2, name: "AirPods Pro", new: true },
+      { id: 3, name: "AirPods", new: false },
+      { id: 4, name: "HomePod", new: false },
+      { id: 5, name: "iPod touch", new: false },
+      { id: 10, name: "Music Accessories", new: false },
+      { id: 5, name: "Gift Cards", new: false }
+    ]
+  }
+];
+
+export default productData;
