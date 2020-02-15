ef7d5666d30d91f001fb5944d6f4ab4c6553e362
rabah babaci <4rabah@gmail.com>
2020-02-14 18:09:54 -0800

    import route and link to home component

--- apple-nav/src/Components/Home.js
+++ apple-nav/src/Components/Home.js
@@ -1,5 +1,14 @@
 import React from "react";
+import { Route, NavLink } from "react-router-dom";
 
-export default function Home() {
-  return <h1>Home</h1>;
+export default function Home({ products }) {
+  console.log(products);
+
+  return (
+    <div className="nav-wrapper">
+      {products.map(product => (
+        <h2>{product.name}</h2>
+      ))}
+    </div>
+  );
 }
