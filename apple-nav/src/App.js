5e3ebe07cbe6f1016b01bde1711dd4da6a593623
rabah babaci <4rabah@gmail.com>
2020-02-14 17:57:16 -0800

    import apple data to the app

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -1,7 +1,10 @@
 import React from "react";
+
 import "./App.css";
+import appleData from "./data/appleData";
 
 function App() {
+  console.log(appleData);
   return (
     <div className="App">
       <h2>test</h2>
