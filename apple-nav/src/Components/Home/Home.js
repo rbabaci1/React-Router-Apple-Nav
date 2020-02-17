a2c5edd33390d8af6741c8ab2a215993cebbb9a7
rabah babaci <4rabah@gmail.com>
2020-02-16 17:46:28 -0800

    add css transition group to the page

--- apple-nav/src/Components/Home/Home.js
+++ apple-nav/src/Components/Home/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, NavLink, Switch } from "react-router-dom";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 import "../Home/home.css";
 import Repository from "../Repository/Repository";
@@ -33,18 +34,28 @@ export default function Home({ products, history }) {
         </div>
       </div>
 
-      <Switch>
-        <Route
-          exact
-          path="/:category"
-          render={props => <Repository {...props} products={products} />}
-        />
+      <Route
+        render={({ location }) => (
+          <TransitionGroup>
+            <CSSTransition key={location.key} timeout={500} classNames="fade">
+              <Switch>
+                <Route
+                  exact
+                  path="/:category"
+                  render={props => (
+                    <Repository {...props} products={products} />
+                  )}
+                />
 
-        <Route
-          path={`/:category/:item`}
-          render={props => <Item {...props} products={products} />}
-        />
-      </Switch>
+                <Route
+                  path={`/:category/:item`}
+                  render={props => <Item {...props} products={products} />}
+                />
+              </Switch>
+            </CSSTransition>
+          </TransitionGroup>
+        )}
+      />
     </div>
   );
 }
